5db98cbf9c7539c4edd62a95928525a6dd2ba5fd
Mahmoud El Shenawy <mahmoud.elshenawy@intcore.com>
2024-05-13 11:35:06 +0300

    Add Nullable Safety to Widget Binding Instance

--- lib/ui/solevato_chat_page.dart
+++ lib/ui/solevato_chat_page.dart
@@ -376,7 +376,7 @@ class _SolevatoChatState extends State<SolevatoChat> {
     final index = _messages.indexWhere((element) => element.id == message.id);
     final updatedMessage = _messages[index].copyWith(previewData: previewData);
 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       setState(() {
         _messages[index] = updatedMessage;
       });
@@ -392,7 +392,7 @@ class _SolevatoChatState extends State<SolevatoChat> {
       return;
     }
 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       setState(() {
         _messages[index] = message;
       });
@@ -404,7 +404,7 @@ class _SolevatoChatState extends State<SolevatoChat> {
   ) {
     final index = _messages.indexWhere((element) => element.id == message.id);
 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       setState(() {
         _messages[index] = message;
       });
